1fe671410fe5d7c0f80a9b737a8f093e7a250027
biogod2020 <biogod2020@gmail.com>
2025-12-01 02:10:20 +0800

    feat: Update site URL and base path in Astro config; add GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://jay-s-blog.github.io',
+  site: 'https://biogod2020.github.io',
+  base: '/Jay-s-Blog',
   integrations: [tailwind(), sitemap()],
 });
